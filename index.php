3243eb4e28ab5169c5e857b847c9b61c6c970946
Juan Garcia <jg845@cornell.edu>
2017-04-04 14:52:36 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="styling/stylesheet.css">
+		<title>Juan Garcia</title>
+	</head>
+	<body>
+		<p1>Hello world</p1>
+	</body>
+</html>
